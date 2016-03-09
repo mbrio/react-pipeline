9979d9e8bd6ddd607fb1a1db084c3ea4e41e6aef
Michael Diolosa <michael.diolosa@gmail.com>
2016-03-08 22:13:04 -0500

    Minor adjustment to lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,11 @@ module.exports = {
     },
     "extends": ["eslint:recommended", "plugin:react/recommended"],
     "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
     "plugins": [
         "react"
     ],
